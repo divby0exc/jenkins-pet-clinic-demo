a2b7fc96c98c42adc123a5cc4aea37acd326b8c2
divby0exc <seddigh.daniel@gmail.com>
2023-09-13 14:52:23 +0200

    created controller, repository and interface, and service class

---
+++ src/main/java/com/divby0exc/petclinic/repository/IRepository.java
@@ -0,0 +1,8 @@
+package com.divby0exc.petclinic.repository;
+
+import com.divby0exc.petclinic.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IRepository extends JpaRepository<Customer, Long> {
+    Customer all
+}
---
+++ src/main/java/com/divby0exc/petclinic/repository/Repository.java
@@ -0,0 +1,7 @@
+package com.divby0exc.petclinic.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Repository implements {
+    @Autowired
+}
