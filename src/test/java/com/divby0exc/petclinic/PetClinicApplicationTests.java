f678c383f627f4fcac3ae6867abb012fe6416d22
divby0exc <seddigh.daniel@gmail.com>
2023-09-13 14:01:05 +0200

    first commit

---
+++ src/test/java/com/divby0exc/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divby0exc.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
