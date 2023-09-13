f75322d5009dcc3f85cb069a7f03ddc423210877
divby0exc <seddigh.daniel@gmail.com>
2023-09-13 15:19:24 +0200

    added fields in customer

--- src/main/java/com/divby0exc/petclinic/model/Customer.java
+++ src/main/java/com/divby0exc/petclinic/model/Customer.java
@@ -1,4 +1,9 @@
 package com.divby0exc.petclinic.model;
 
 public class Customer {
+    private String name;
+    private String address;
+    private String city;
+    private String number;
+    private String petName;
 }
