d5e7918f9b355d8aa8c86429ca6f81bbdd31fd03
divby0exc <seddigh.daniel@gmail.com>
2023-09-13 15:41:58 +0200

    added connection to h2 in props and changed repo to irepository

--- src/main/java/com/divby0exc/petclinic/repository/IRepository.java
+++ src/main/java/com/divby0exc/petclinic/repository/IRepository.java
@@ -2,9 +2,11 @@ package com.divby0exc.petclinic.repository;
 
 import com.divby0exc.petclinic.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 
-public interface IRepository {
+@Repository
+public interface IRepository  extends JpaRepository<Customer, Long> {
 }
