8e8562899616158d49f3d5f3deb082dd8777a42a
divby0exc <seddigh.daniel@gmail.com>
2023-09-13 15:27:48 +0200

    weird about implementation

--- src/main/java/com/divby0exc/petclinic/repository/IRepository.java
+++ src/main/java/com/divby0exc/petclinic/repository/IRepository.java
@@ -3,5 +3,10 @@ package com.divby0exc.petclinic.repository;
 import com.divby0exc.petclinic.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
+
 public interface IRepository extends JpaRepository<Customer, Long> {
+    @Override
+    List<Customer> findAll();
 }
--- src/main/java/com/divby0exc/petclinic/repository/Repository.java
+++ src/main/java/com/divby0exc/petclinic/repository/Repository.java
@@ -1,4 +1,10 @@
 package com.divby0exc.petclinic.repository;
 
+import com.divby0exc.petclinic.model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@org.springframework.stereotype.Repository
 public class Repository {
+
+
 }
